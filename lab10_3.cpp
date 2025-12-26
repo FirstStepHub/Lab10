e080d8c1ccc32bc914788bb067325df41d3ccf35
FirstStepHub <first_yo1@hotmail.com>
2025-12-26 17:43:41 +0700

    lab10_3_complete.cpp

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,26 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include<fstream>
+#include<cstdlib>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+int main(){
+    int count = 0 ;
+    double sum = 0 ;
+    double sum_squr = 0 ;
+    string textline ;
+    ifstream source("score.txt") ;
+    while (getline(source,textline))
+    {
+        sum += atof(textline.c_str()) ;
+        sum_squr += pow((atof(textline.c_str())),2) ;
+        count++ ;
+    }
+    cout << "Number of data = " << count << endl;
+    cout << setprecision(3);
+    cout << "Mean = " << sum/count << endl;
+    cout << "Standard deviation = " << sqrt((sum_squr/count) - pow(sum/count, 2)) ;
+}
